36ff954483aee8887f1e7f7c39eb913fa0288c9e
gabr-silva <gabrielvieira.s0801@gmail.com>
2023-09-18 12:44:35 -0300

    animacao carregamento

--- src/telas/inicial/index.js
+++ src/telas/inicial/index.js
@@ -1,18 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { View, Image } from "react-native";
 import BotaoInicial from "../../components/BotaoInicio";
 import { auth } from "../../config/firebase";
+import AnimacaoCarregando from '../../../assets/AnimacaoCarregando.gif'
+import style from "./style";
 
 export default function Inicio({navigation}) {
-useEffect(() =>{
-    const estadoUsuario = auth.onAuthStateChanged(
-        usuario => {
-            if(usuario){
-                navigation.replace('Modulo')
-            }
-        })
-    return () => estadoUsuario();
-}, [])
+    const [carregando, setCarregando] = useState(true)
 
+    useEffect(() =>{
+        const estadoUsuario = auth.onAuthStateChanged(
+            usuario => {
+                if(usuario){
+                    navigation.replace('Modulo')
+                }
+                setCarregando(false)
+            })
+        return () => estadoUsuario();
+    }, [])
+
+    if (carregando) {
+        return (
+            <View style ={style.containerAnimacao}>
+                <Image source ={AnimacaoCarregando} 
+                    style={style.imagem}/>
+            </View>
+        )
+    }
     return(
     <>
         <BotaoInicial onPress={() => navigation.navigate('Login')}>Login</BotaoInicial>
---
+++ src/telas/inicial/style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    containerAnimacao:{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    imagem: {
+        width: 200,
+        height: 200
+    }
+})
