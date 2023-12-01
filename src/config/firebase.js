894f0e85439e2355ed6c9da465d8d3bfb51eefe6
gabr-silva <gabrielvieira.s0801@gmail.com>
2023-11-30 21:23:26 -0300

    versao de teste

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore';
 
-import {
+/*import {
     API_KEY,
     AUTH_DOMAIN,
     DATABASE_URL,
@@ -23,8 +23,20 @@ const firebaseConfig = {
     messagingSenderId: MESSAGING_SENDER_ID,
     appId: APP_ID,
     measurementId: MEASUREMENT_ID,
+};*/
+
+//banco da isa
+const firebaseConfig = {
+    apiKey: "AIzaSyB-PwQjqA05kwdqEVwzhiy8slOfT1OVOQk",
+    authDomain: "libramos-bd.firebaseapp.com",
+    projectId: "libramos-bd",
+    storageBucket: "libramos-bd.appspot.com",
+    messagingSenderId: "1094313246508",
+    appId: "1:1094313246508:web:26991f4712528ea005f689",
+    measurementId: "G-2E4M0NSL3S"
 };
 
+//banco oficial
 /*const firebaseConfig = {
     apiKey: "AIzaSyBp1FkFOfNFmKs2x4pOg33sqkthwJ-RYas",
     authDomain: "libramos-teste.firebaseapp.com",
